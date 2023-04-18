53e8f31e9293f6ad0643c5744b4028a943e74a3c
ArturLabenskyy <dodik.dod@hotmail.com>
2023-04-18 18:28:22 +0300

    netlify error fix

--- src/components/Login/Login.component.js
+++ src/components/Login/Login.component.js
@@ -3,19 +3,24 @@ import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 
 import Wrapped from "./Login.styled";
+import Loader from "../Loader/Loader.component";
+
 import mongoApi from "../../services/api/db.api";
 import { useAuthContext } from "../../context/auth.context";
+import { useGamesContext } from "../../context/games.context";
 
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const { isFetching, setFetching } = useGamesContext();
     const { setLogin, getCurrentUser } = useAuthContext();
 
     const navigate = useNavigate();
 
     const loginHandler = async () => {
         try {
+            setFetching(true);
             const res = await mongoApi.post("auth/login", {
                 email: email,
                 password: password,
@@ -28,38 +33,45 @@ const Login = () => {
                 setLogin(true);
                 const userId = await getCurrentUser();
                 localStorage.setItem("userId", JSON.stringify(userId._id));
+                setFetching(false);
                 navigate("/");
             }
         } catch (error) {
+            setFetching(false);
+
             console.log(error);
         }
     };
 
     return (
         <Wrapped>
-            <div className="login-form column">
-                <label>Enter Your Email</label>
-                <input
-                    type="email"
-                    name="email"
-                    placeholder="Enter your email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
+            {isFetching ? (
+                <Loader />
+            ) : (
+                <div className="login-form column">
+                    <label>Enter Your Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Enter your email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
 
-                <label>Enter Your Password</label>
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="Enter your password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
+                    <label>Enter Your Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Enter your password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
 
-                <button className="finish" onClick={() => loginHandler()}>
-                    Login
-                </button>
-            </div>
+                    <button className="finish" onClick={() => loginHandler()}>
+                        Login
+                    </button>
+                </div>
+            )}
         </Wrapped>
     );
 };
